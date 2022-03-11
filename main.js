06a2899a135617bd820af882afc8167e4d11c163
Kat Sundquist <ksundquist@pcconfigs-MacBook-Pro.local>
2022-03-11 11:18:01 -0800

    Added correct guess functionality

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-const submitGuess = document.getElementById("word-in");
+const submitWordToGuess = document.getElementById("word-in");
+const submitLetterGuess = document.getElementById("letter-in");
+const concealedWordElement = document.getElementById("word-in-play");
 let wordToGuess;
+let letterGuess;
+let concealedWord = "";
 
 function setWord () {
 
-    const wordToGuessElement = document.getElementById("word-to-guess")
+    const wordToGuessElement = document.getElementById("word-to-guess");
     wordToGuess = wordToGuessElement.value;
     //console.log("in setWord", wordToGuess.length);
 
     wordToGuessElement.value = '';
 
+    console.log(wordToGuess);
+
     concealWord();
 }
 
 
-submitGuess.addEventListener('click', setWord);
-console.log("boo");
+submitWordToGuess.addEventListener('click', setWord);
 
 
 function concealWord() {
@@ -23,15 +28,9 @@ function concealWord() {
 
     const letterPlaceholder = "_ "
 
-    let concealedWord = "";
-
     for(let i = 0; i < wordLength; i++){
         concealedWord += letterPlaceholder;
     }
-
-    console.log("after loop", concealedWord);
-
-    const concealedWordElement = document.getElementById("word-in-play");
 
     concealedWordElement.innerHTML = concealedWord;
 }
@@ -43,6 +42,56 @@ function concealWord() {
 //      display letter in letter graveyard
 //      display body part
 
+function setGuess() {
+    const letterGuessElement = document.getElementById("guess");
+    letterGuess = letterGuessElement.value;
+
+    letterGuessElement.value = '';
+
+    compareGuess();
+}
+
+function compareGuess(){
+
+    letterGuess = letterGuess.toLowerCase();
+    console.log(letterGuess);
+
+    if(wordToGuess.includes(letterGuess)){
+        addCorrectGuess();
+    } else {
+        console.log("SPLAT");
+    }
+}
+
+function addCorrectGuess(){
+    console.log(wordToGuess);
+    console.log(concealedWord);
+
+
+    // _ are at even indicies
+    let letterIndicies = [];
+    for(i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess[i] === letterGuess){
+            letterIndicies.push(i * 2);
+        }
+    }
+
+    console.log(letterIndicies);
+
+    for(i = 0; i < letterIndicies.length; i++){
+        concealedWord = replaceAtIndex(concealedWord, letterIndicies[i], letterGuess);
+        concealedWordElement.innerHTML = concealedWord;
+    }
+    console.log(concealedWord);
+}
+
+function replaceAtIndex(string, index, newValue) {
+    return string.substring(0,index) + newValue + string.substring(index+1);
+}
+
+
+
+submitLetterGuess.addEventListener('click', setGuess);
 
 let bodyParts = document.querySelectorAll(".body-photo");
 
