ea47215e6cbff7fb751d595236f79d1c555e3ec5
Kat Sundquist <ksundquist@pcconfigs-MacBook-Pro.local>
2022-03-18 10:49:34 -0700

    Got part of conceal word working, but need to refactor some stuff

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const submitWordToGuess = document.getElementById("word-in");
 const submitLetterGuess = document.getElementById("letter-in");
 const concealedWordElement = document.getElementById("word-in-play");
+const guessedLetters = document.getElementById("guessed-letters");
 let wordToGuess;
 let letterGuess;
 let concealedWord = "";
@@ -71,6 +72,21 @@ function addCorrectGuess(){
         concealedWord = replaceAtIndex(concealedWord, letterIndicies[i], letterGuess);
         concealedWordElement.innerHTML = concealedWord;
     }
+
+    // checking for a win from here down.  This is gross, and I want to change
+    // it when I refactor because this is disgusting.
+    let tally = 0;
+    for( i = 0; i < concealedWord.length; i++){
+        if(concealedWord[i] === "_"){
+            tally += 1;
+        } 
+    }
+
+    if(tally === 0){
+        setTimeout(function() {
+            window.alert("Hooray!  You win!");
+          }, 9)
+    }
 }
 
 function replaceAtIndex(string, index, newValue) {
@@ -78,7 +94,7 @@ function replaceAtIndex(string, index, newValue) {
 }
 
 function addIncorrectGuess(){
-    const guessedLetters = document.getElementById("guessed-letters");
+    
     
     const span = document.createElement("span");
     const guess = document.createTextNode(letterGuess);
@@ -89,6 +105,12 @@ function addIncorrectGuess(){
     let guessCount = guessedLetters.querySelectorAll("span").length;
 
     showBodyPart(guessCount);
+
+    if (guessCount >= 6){
+        setTimeout(function() {
+            window.alert("You Lose :(");
+          }, 9)
+    }
 }
 
 submitLetterGuess.addEventListener('click', setGuess);
@@ -101,4 +123,40 @@ function showBodyPart(guessCount) {
 
     part.classList.remove("invisible");
     part.classList.add("visible");
+}
+
+
+var playAgain = document.getElementById("play-again");
+
+playAgain.onclick = function () {
+
+  
+    concealedWordElement.innerHTML = "_ _ _ _ _ _";
+    wordToGuess = "";
+
+    letters = guessedLetters.querySelectorAll("span");
+
+    letters.forEach(function (letter) {
+        letter.remove();
+    })
+
+    // hide the body parts
+
+    let bodyPhotos = document.querySelectorAll(".body-photo");
+
+    bodyPhotos.forEach(function (part){
+        part.classList.remove("visible");
+        part.classList.add("invisible");
+        console.log("in here");
+    })
+
+
+//   // changes the squares background color to white
+//   squareList.forEach(function (square) {
+//     square.style.backgroundColor = "white";
+//     square.innerHTML = "";
+//   })
+//   single.disabled = false;
+//   multi.disabled = false;
+
 }
